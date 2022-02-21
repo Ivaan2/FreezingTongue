faae16b8e709ce24aff7638db6da0cae8d37af7b
Ivaan2 <ivan.m.m.2002.ivi@gmail.com>
2022-02-21 16:56:38 +0100

    Iván: Alertas y backend Pablo: Spalsh Screen Fran: Layout Heladería

--- src/main/java/com/example/freezingtongue/HelloApplication.java
+++ src/main/java/com/example/freezingtongue/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login_screen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 300, 300);
+        stage.setTitle("Freezing Tongue");
         stage.setScene(scene);
         stage.show();
     }
