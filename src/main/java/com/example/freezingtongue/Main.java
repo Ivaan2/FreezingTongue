ace9c865679f2f43d6947717c937fbcdfeaaf073
Pablo-PR <pabloparrarodriguez@gmail.com>
2022-02-22 10:19:09 +0100

    Carpeta images

--- src/main/java/com/example/freezingtongue/Main.java
+++ src/main/java/com/example/freezingtongue/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login_screen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login_screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 300, 300);
         stage.setTitle("Freezing Tongue");
         stage.setScene(scene);
