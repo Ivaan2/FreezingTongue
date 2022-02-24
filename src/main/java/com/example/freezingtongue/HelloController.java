46649f2ecf84b06aff5ad420d3c4423558a98adb
Ivaan2 <i.morales.mellado@iescristobaldemonroy.es>
2022-02-24 14:44:44 +0100

    Ivan: Login y Registrar Pablo: Splash Screen Fran: Interfaz helado

--- src/main/java/com/example/freezingtongue/HelloController.java
+++ src/main/java/com/example/freezingtongue/HelloController.java
@@ -1,14 +1,47 @@
 package com.example.freezingtongue;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+
+    private String valorUsuario;
+    private String contrasenna;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField loginUsuario;
+    @FXML
+    private TextField loginContrasenna;
+
+    @FXML
+    private void Login(ActionEvent event){
+        this.valorUsuario = loginUsuario.getText();
+        this.contrasenna = loginContrasenna.getText();
+        System.out.println("Usuario " + valorUsuario + " Contraseña: " + contrasenna);
+        mostrarMenuAdministrador(event);
+    }
+    @FXML
+    public void mostrarMenuAdministrador(Event event) {
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getResource("reset_password.fxml"));
+            Scene scene = new Scene(root);
+            Stage appStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            appStage.setScene(scene);
+            appStage.toFront();
+            appStage.show();
+
+        } catch (Exception e) {
+        }
     }
 }
